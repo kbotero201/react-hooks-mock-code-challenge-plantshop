ae066e79eeff6c52fb2ded1850216323481a7317
Kevin Botero <kbotero201@gmail.com>
2021-01-19 19:57:17 -0500

    finished features

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+
+  const [plants, setPlants] = useState([])
+  const[search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(resp => resp.json())
+    .then(data => 
+      setPlants(data)
+    )
+  },[])
+
+  function addNewPlant(newPlant){
+    setPlants([newPlant, ...plants])
+  }
+
+  let filteredSearch = plants.filter((plant)=> 
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  )
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addNewPlant={addNewPlant} />
+      <Search search={search} setSearch={setSearch} />
+      <PlantList plants={filteredSearch} />
     </main>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Search() {
+function Search({setSearch, search}) {
+
+
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
       <input
+        value={search}
+        onChange={(e)=> setSearch(e.target.value)}
         type="text"
         id="search"
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
       />
     </div>
   );
